af799f5758dbef419c632663baeb04db275c1915
FreGitHu <101549035+FreGitHu@users.noreply.github.com>
2024-02-16 09:02:57 +0000

    Fix Typo

--- src/main.c
+++ src/main.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main(int argc, char *argv[]) {
     if (argc == 0) {
-        printf("\033[1;33mWARNING: \033[0m\033[1mNo Program Was Provided, This Could Be An Issue OR An Attacker Trying TO HACK Your Computer. If you are not sure exit this program RIGHT NOW\033[0m");
+        printf("\033[1;33mWARNING: \033[0m\033[1mNo Program Name Was Provided, This Could Be An Issue OR An Attacker Trying TO HACK Your Computer. If you are not sure exit this program RIGHT NOW\033[0m");
     }
     printf("Boxy Compiler (aka CBoxy I guess). Pull request what you want to be in this message (no normal stuff)");
 
